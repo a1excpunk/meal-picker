b4543d0595bb833cd63d294c0409d79b81b1f3a8
Alex <alexandrasauri19@gmail.com>
2021-10-05 18:18:35 +0400

    feat: add 'view all meals' and 'delete individually' functionality

--- main.js
+++ main.js
@@ -13,19 +13,23 @@ const ADDED_MEAL_SUCCESS = document.getElementById('added-success');
 const DELETE_ALL_MEAL = document.getElementById('btn-delete-meals');
 const DELETE_ALL_MEAL_MESSAGE = document.getElementById('delete-message');
 
+const SHOW_EVERY_MEAL = document.getElementById('every-meal');
+const SHOW_EVERY_MEAL_BTN = document.getElementById('btn-show-every-meal');
+
 const BMI_RESULT = document.getElementById('bmi-result');
 const BMI_WEIGHT_INPUT = document.getElementById('weight');
 const BMI_HEIGHT_INPUT = document.getElementById('height');
 const BMI_CALCULATE_BTN = document.getElementById('btn-bmi-calculate');
 const BMI_CLEAR_BTN = document.getElementById('btn-bmi-clear');
 
+let message = document.createElement("p");
 
 let meals = localStorage.getItem(0);
-let allMeals ;
+let allMeals;
 
-if(meals ===null){
+if (meals === null) {
     allMeals = [];
-}else{
+} else {
     allMeals = JSON.parse(meals);
 }
 
@@ -41,22 +45,55 @@ function bmiFormula(weight, height) {
     return (weight / (height ** 2) * 10000).toFixed(1);
 }
 
-function redMessage(element){
+function redMessage(element) {
     element.style.backgroundColor = '#ffbebe';
     element.style.color = '#c20000';
     element.style.fontSize = '14px'
 }
 
-function greenMessage(element){
+function greenMessage(element) {
     element.style.backgroundColor = '#d4ffdd';
     element.style.color = '#00af26';
     element.style.fontSize = '14px'
 }
 
-function initialStyle (element){
+function initialStyle(element) {
     element.style.backgroundColor = 'initial'
     element.style.color = 'initial'
     element.style.fontSize = 'initial'
+}
+
+function everyMealInStorage() {
+    const classes = ['btn', 'btn-remove-meal'];
+    let i;
+    let mealContainer, meal, removeBtn;
+
+    allMeals.map((el) => {
+        mealContainer = document.createElement('div');
+        meal = document.createElement('p')
+        removeBtn = document.createElement('button');
+        removeBtn.addEventListener("click", function (e) {
+            allMeals.forEach((element, index) => {
+                if (e.target.previousSibling.innerText.toLowerCase() === element) {
+                    allMeals.splice(index, 1);
+                    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+                    localStorage.setItem(0, JSON.stringify(allMeals));
+                }
+            });
+        });
+
+        removeBtn.classList.add(...classes);
+        mealContainer.classList.add("meal-container");
+
+        removeBtn.innerText = 'Remove';
+        meal.innerText = el
+
+        mealContainer.append(meal);
+        mealContainer.append(removeBtn);
+        SHOW_EVERY_MEAL.append(mealContainer);
+
+    });
+
 }
 
 PICKER_BTN.addEventListener('click', () => {
@@ -73,27 +110,26 @@ PICKER_BTN.addEventListener('click', () => {
 
 SEARCH_BTN.addEventListener('click', () => {
     if (!INGREDIENTS_INPUT.value) {
-        MATCHED_MEAL.innerText = "forgot to write key word"
-        redMessage(MATCHED_MEAL)
+        MATCHED_MEAL.innerText = "forgot to write key word";
+        redMessage(MATCHED_MEAL);
     } else if (searchIngredients(INGREDIENTS_INPUT.value).every((val) => val === null)) {
-        MATCHED_MEAL.innerText = "there is no meal to match or you spelled it wrong"
-        redMessage(MATCHED_MEAL)
-
+        MATCHED_MEAL.innerText = "there is no meal to match or you spelled it wrong";
+        redMessage(MATCHED_MEAL);
     }
     else if (searchIngredients(INGREDIENTS_INPUT.value) !== null) {
-        initialStyle(MATCHED_MEAL)
+        initialStyle(MATCHED_MEAL);
         if (MATCHED_MEAL.hasChildNodes()) {
-            MATCHED_MEAL.innerHTML = ''
+            MATCHED_MEAL.innerHTML = '';
         }
 
         for (let i = 0; i < searchIngredients(INGREDIENTS_INPUT.value).filter(matchedMeal => matchedMeal !== null).length; i++) {
-            let mealContainer = document.createElement('p');
+            const mealContainer = document.createElement('p');
             MATCHED_MEAL.appendChild(mealContainer);
-            mealContainer.innerText = `${i + 1}) ${searchIngredients(INGREDIENTS_INPUT.value).filter(matchedMeal => matchedMeal !== null)[i]['input']}`
+            mealContainer.innerText = `${i + 1}) ${searchIngredients(INGREDIENTS_INPUT.value).filter(matchedMeal => matchedMeal !== null)[i]['input']}`;
 
         }
         console.log(searchIngredients(INGREDIENTS_INPUT.value));
-        INGREDIENTS_INPUT.value = ''
+        INGREDIENTS_INPUT.value = '';
     }
 });
 
@@ -116,7 +152,7 @@ BMI_CLEAR_BTN.addEventListener('click', () => {
     BMI_HEIGHT_INPUT.value = ''
     BMI_WEIGHT_INPUT.value = ''
     BMI_RESULT.innerText = '0'
-})
+});
 
 ADD_MEAL_BTN.addEventListener('click', () => {
 
@@ -134,8 +170,21 @@ ADD_MEAL_BTN.addEventListener('click', () => {
     ADDED_MEAL_SUCCESS.classList.remove('hide');
     setTimeout(() => ADDED_MEAL_SUCCESS.classList.add('hide'), 2000);
 
-})
+});
 
 DELETE_ALL_MEAL.addEventListener('click', () => {
     localStorage.clear()
-})
+    window.location.reload(true);
+});
+
+SHOW_EVERY_MEAL_BTN.addEventListener('click', () => {
+    SHOW_EVERY_MEAL.innerText = "";
+    everyMealInStorage();
+    if (allMeals[0] === undefined) {
+        SHOW_EVERY_MEAL.appendChild(message);
+        message.innerText = 'there is no meal, add some first';
+        message.style.padding = "10px 0"
+        redMessage(message);
+    }
+});
+
